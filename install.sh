1a6e3a759fe397721641016c35aabca1eedd4d0b
HaoyiZhu <zhuhaoyi@sjtu.edu.cn>
2022-02-16 18:33:49 +0800

    update doc

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ conda activate alphatracker
 # conda env update --file environment.yml
 
 # Install pytorch
-conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
+conda install pytorch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0 cudatoolkit=11.1 -c pytorch -c conda-forge
 
 export PATH=/usr/local/cuda/bin/:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64/:$LD_LIBRARY_PATH
