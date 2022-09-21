6167339ecb7657cd0b805856d2b8b12b7dd27bb2
Fred Fang <510641426@qq.com>
2022-09-21 20:23:17 +0800

    Update install3.sh

--- scripts/install3.sh
+++ scripts/install3.sh
@@ -7,4 +7,5 @@ export PATH=/usr/local/cuda/bin/:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64/:$LD_LIBRARY_PATH
 pip uninstall alphatracker -y
 pip uninstall alphatracker -y
+pip install Cython
 python setup.py build develop
